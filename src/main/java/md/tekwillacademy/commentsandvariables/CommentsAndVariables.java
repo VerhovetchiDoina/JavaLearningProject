3b7d6291c5fdbb48480c55f3cba939f6ab93ee3d
Radu-Oprisoreanu Doina <dointasavca@gmail.com>
2024-04-14 12:48:59 +0300

    added the primitive data type variables as a practical exercise

---
+++ src/main/java/md/tekwillacademy/commentsandvariables/CommentsAndVariables.java
@@ -0,0 +1,37 @@
+package md.tekwillacademy.commentsandvariables;
+
+public class CommentsAndVariables {
+    public static void main(String[] args) {
+        //Declare the primitive data type variables
+    boolean homeWorkIsDone; // declaration of a variable of type boolean
+        homeWorkIsDone = true; // assign a valueW
+      System.out.println("The value of my variable is: " + homeWorkIsDone);
+    byte numberByte; // declaration of a variable of type byte
+        numberByte = 95; // assign a value
+      System.out.println("The value of the byte variable is: " + numberByte);
+    char letter; // declaration of a variable of type char
+        letter = 'D'; // assign a value
+      System.out.println("The value of the char variable is: " + letter);
+    short bigNumber; // assign a value
+        bigNumber = 32000; // declaration of a variable of type short
+      System.out.println("The value of the short variable is: " + bigNumber);
+    int age; // declaration of a variable of type int
+        age = 28; // assign a value
+      System.out.println("The value of the int variable is: " + age);
+    long worldPopulation; // declaration of a variable of type long
+        worldPopulation = 7795000000L; // assign a value
+      System.out.println("The world population is: " + worldPopulation);
+    float pi; // declaration of a variable of type float
+        pi = 3.14f; // assign a value
+      System.out.println("The pi is: " +pi);
+    double quantityFlowers; // declaration of a variable of type double
+        quantityFlowers = 100.75; // assign a value
+      System.out.println("The amount of flowers is: " + quantityFlowers + " square meters");
+    String name; // declaration of a variable of type String
+         name = "Verhovetchi"; // assign a value
+        System.out.println("The name is: " +name);
+    Object object; // declaration of a variable of type Object
+        object = "Hi!"; // assign a value
+        System.out.println("The object is: " +object);
+    }
+}
