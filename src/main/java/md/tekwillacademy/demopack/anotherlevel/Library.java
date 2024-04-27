f5e95d395e88c0681cbea75347886155a17029aa
Radu-Oprisoreanu Doina <dointasavca@gmail.com>
2024-04-27 07:56:05 +0300

    Added the demopack for the LibraryService

---
+++ src/main/java/md/tekwillacademy/demopack/anotherlevel/Library.java
@@ -0,0 +1,15 @@
+package md.tekwillacademy.demopack.anotherlevel;
+
+public class Library {
+    public int numberOfBooks;
+
+    public int numberOfSellers;
+
+    public String address;
+
+    public Seller seller;
+
+
+    static boolean hasEmployee = true;
+
+}
